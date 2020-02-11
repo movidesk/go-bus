aeb54630b041a9883d784224f6a0aa9ee2719f0d
Silvio Greuel <silviogreuel@gmail.com>
2020-02-11 18:14:24 -0300

    Publishing with publish confirms

--- amqp/publisher.go
+++ amqp/publisher.go
@@ -1,16 +1,20 @@
 package amqp
 
 import (
+	"encoding/json"
+	"log"
+	"sync"
+
 	base "github.com/movidesk/go-bus"
 	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
-	"sync"
 )
 
 type PublisherOptionsFn func(*PublisherOptions)
 
 type PublisherOptions struct {
-	confirms bool
+	confirm       bool
+	confirmations chan amqp.Confirmation
 
 	exchange string
 	key      string
@@ -22,15 +26,55 @@ type PublisherOptions struct {
 	wg    *sync.WaitGroup
 }
 
-func SetPublisherWaitGroup(wg *sync.WaitGroup) PublisherOptionsFn {
+func SetPublisherConfirm(confirm bool) PublisherOptionsFn {
 	return func(o *PublisherOptions) {
-		o.wg = wg
+		o.confirm = confirm
+	}
+}
+
+func SetPublisherConfirmations(confirmations chan amqp.Confirmation) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.confirmations = confirmations
+	}
+}
+
+// SetPublisherExchange specifies the name of the exchange to publish to. The exchange name can be empty, meaning the default exchange. If the exchange name is specified, and that exchange does not exist, the server will raise a channel exception.
+func SetPublisherExchange(exchange string) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.exchange = exchange
+	}
+}
+
+// SetPublisherKey specifies the routing key for the message. The routing key is used for routing messages depending on the exchange configuration.
+func SetPublisherKey(key string) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.key = key
+	}
+}
+
+// SetPublisherMandatory this flag tells the server how to react if the message cannot be routed to a queue. If this flag is set, the server will return an unroutable message with a Return method. If this flag is zero, the server silently drops the message.
+func SetPublisherMandatory(mandatory bool) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.mandatory = mandatory
+	}
+}
+
+// SetPublisherImmediate this flag tells the server how to react if the message cannot be routed to a queue consumer immediately. If this flag is set, the server will return an undeliverable message with a Return method.
+func SetPublisherImmediate(immediate bool) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.immediate = immediate
 	}
 }
 
 func SetPublisherClose(close <-chan struct{}) PublisherOptionsFn {
 	return func(o *PublisherOptions) {
 		o.close = close
+	}
+}
+
+func SetPublisherWaitGroup(wg *sync.WaitGroup) PublisherOptionsFn {
+	return func(o *PublisherOptions) {
+		o.wg = wg
 	}
 }
 
@@ -44,20 +88,63 @@ type pub struct {
 
 func NewPublisher(sess *Session, fns ...PublisherOptionsFn) (Publisher, error) {
 	o := &PublisherOptions{}
+	SetPublisherMandatory(false)(o)
+	SetPublisherImmediate(false)(o)
+	SetPublisherConfirm(true)(o)
+	SetPublisherConfirmations(make(chan amqp.Confirmation, 1))(o)
 	for _, fn := range fns {
 		fn(o)
 	}
 
-	return &pub{
+	p := &pub{
 		Session:          sess,
 		PublisherOptions: o,
-	}, nil
+	}
+
+	return p, p.setup()
 }
 
-func (p *pub) Publish(base.Message) (error, bool) {
-	err := p.Session.Publish(p.exchange, p.key, p.mandatory, p.immediate, amqp.Publishing{})
+func (p *pub) Publish(msg base.Message) (error, bool) {
+	pmsg, err := parse(msg)
+	if err != nil {
+		return err, false
+	}
+
+	err = p.Session.Publish(p.exchange, p.key, p.mandatory, p.immediate, pmsg)
 	if err != nil {
 		return errors.Wrap(err, "Could not Session.Publish()"), false
 	}
-	return nil, false
+
+	//TODO: confirmation timeout
+	c := <-p.confirmations
+	return nil, c.Ack
+}
+
+func (p *pub) setup() error {
+	if !p.confirm {
+		close(p.confirmations)
+		return nil
+	}
+
+	err := p.Confirm(false)
+	if err != nil {
+		log.Printf("publisher confirms not supported")
+		close(p.confirmations)
+	} else {
+		p.NotifyPublish(p.confirmations)
+	}
+	return err
+}
+
+func parse(msg base.Message) (amqp.Publishing, error) {
+	body, err := json.Marshal(msg.Body)
+	if err != nil {
+		return amqp.Publishing{}, errors.Wrap(err, "Could not json.Marshal the message body")
+	}
+
+	headers := amqp.Table(msg.Header)
+	return amqp.Publishing{
+		Body:    body,
+		Headers: headers,
+	}, nil
 }
--- amqp/publisher_test.go
+++ amqp/publisher_test.go
@@ -24,14 +24,90 @@ func (s *PublisherIntegrationSuite) TestNewPublisher() {
 	assert.NotNil(pub)
 }
 
-func (s *PublisherIntegrationSuite) TestPublishWithConfirm() {
+func (s *PublisherIntegrationSuite) TestPublishWithConfirmOnUnexistentExchange() {
 	assert := assert.New(s.T())
 
 	conn, _ := NewConnection()
 	sess, _ := NewSession(conn)
-	pub, _ := NewPublisher(sess)
+	pub, _ := NewPublisher(
+		sess,
+		SetPublisherExchange("unexistent"),
+	)
 
 	err, ok := pub.Publish(base.Message{})
+	assert.NoError(err)
+	assert.False(ok)
+}
+
+func (s *PublisherIntegrationSuite) TestPublishWithConfirmOnExistentExchange() {
+	assert := assert.New(s.T())
+
+	conn, _ := NewConnection()
+	sess, _ := NewSession(conn)
+	pub, _ := NewPublisher(
+		sess,
+		SetPublisherExchange("amq.topic"),
+	)
+
+	err, ok := pub.Publish(base.Message{})
+	assert.NoError(err)
+	assert.True(ok)
+}
+
+func (s *PublisherIntegrationSuite) TestPublishWithoutConfirmOnUnexistentExchange() {
+	assert := assert.New(s.T())
+
+	conn, _ := NewConnection()
+	sess, _ := NewSession(conn)
+	pub, _ := NewPublisher(
+		sess,
+		SetPublisherConfirm(false),
+		SetPublisherExchange("unexistent"),
+	)
+
+	err, ok := pub.Publish(base.Message{})
+	assert.NoError(err)
+	assert.False(ok)
+}
+
+func (s *PublisherIntegrationSuite) TestPublishWithoutConfirmOnExistentExchange() {
+	assert := assert.New(s.T())
+
+	conn, _ := NewConnection()
+	sess, _ := NewSession(conn)
+	pub, _ := NewPublisher(
+		sess,
+		SetPublisherConfirm(false),
+		SetPublisherExchange("amq.topic"),
+	)
+
+	err, ok := pub.Publish(base.Message{})
+	assert.NoError(err)
+	assert.False(ok)
+}
+
+func (s *PublisherIntegrationSuite) TestPublishWithHeaderAndBody() {
+	assert := assert.New(s.T())
+
+	conn, _ := NewConnection()
+	sess, _ := NewSession(conn)
+	pub, _ := NewPublisher(
+		sess,
+		SetPublisherExchange("amq.topic"),
+	)
+
+	type Fruit struct {
+		Name string `json:"name"`
+	}
+
+	err, ok := pub.Publish(base.Message{
+		Header: base.Header{
+			"key-string": "value",
+			"key-int":    1,
+			"key-bool":   true,
+		},
+		Body: &Fruit{"Banana"},
+	})
 	assert.NoError(err)
 	assert.True(ok)
 }
